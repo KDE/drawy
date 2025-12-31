125c9e47502a9c47f132cb7b9d10f2ad3ecfce0d
Laurent Montel <montel@kde.org>
2025-12-31 13:11:53 +0100

    Assign icon directly in main.cpp => we can show it in kwin.
    Use kde macro for installing icons.
    Use correct kde icon naming

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,8 @@ int main(int argc, char *argv[])
 {
     QApplication a{argc, argv};
     QApplication::setStyle(u"Fusion"_s);
+    a.setWindowIcon(QIcon(u":/drawy/drawy.svg"_s));
+
     KCrash::initialize();
 
     QCommandLineParser parser;
