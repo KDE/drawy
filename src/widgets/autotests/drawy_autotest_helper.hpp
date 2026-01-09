3d7dade0b21266a44a8f5eac6837818acd8719e6
Laurent Montel <montel@kde.org>
2026-01-09 07:53:27 +0100

    Allow to compare serialize rectangle

---
+++ src/widgets/autotests/drawy_autotest_helper.hpp
@@ -0,0 +1,20 @@
+/*
+   SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <QString>
+class QJsonDocument;
+class QJsonObject;
+class QJsonArray;
+namespace AutoTestHelper
+{
+void diffFile(const QString &refFile, const QString &generatedFile);
+void compareFile(const QString &repo, const QByteArray &data, const QString &name);
+QJsonDocument loadJsonDocument(const QString &originalJsonFile);
+QJsonObject loadJsonObject(const QString &originalJsonFile);
+QJsonArray loadJsonArrayObject(const QString &originalJsonFile);
+}
--- src/widgets/autotests/rectangleitemtest.cpp
+++ src/widgets/autotests/rectangleitemtest.cpp
@@ -5,10 +5,13 @@
  */
 
 #include "rectangleitemtest.hpp"
+#include "drawy_autotest_helper.hpp"
 #include "item/rectangle.hpp"
+#include <QJsonObject>
 #include <QTest>
-QTEST_MAIN(RectangleItemTest)
 
+QTEST_MAIN(RectangleItemTest)
+using namespace Qt::Literals::StringLiterals;
 RectangleItemTest::RectangleItemTest(QObject *parent)
     : QObject{parent}
 {
@@ -18,6 +21,23 @@ void RectangleItemTest::shouldHaveDefaultValues()
 {
     const RectangleItem i;
     QCOMPARE(i.type(), Item::Type::Rectangle);
+}
+
+void RectangleItemTest::shouldSerialize_data()
+{
+    QTest::addColumn<QString>("name");
+    QTest::addRow("rectangle1") << u"rectangle1"_s;
+}
+
+void RectangleItemTest::shouldSerialize()
+{
+    QFETCH(QString, name);
+
+    const RectangleItem f;
+    const QJsonObject obj = f.serialize();
+    const QJsonDocument doc(obj);
+    const QByteArray ba = doc.toJson();
+    AutoTestHelper::compareFile(u"/rectangle/"_s, ba, name);
 }
 
 #include "moc_rectangleitemtest.cpp"
