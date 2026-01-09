3c34443189a4b1245ff93b0fe797111397d9d5c5
Laurent Montel <montel@kde.org>
2026-01-09 13:12:56 +0100

    Add kaboutdata => we can show license version and co in CLI

--- src/apps/main.cpp
+++ src/apps/main.cpp
@@ -2,10 +2,12 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+#include <KAboutData>
 #include <KCrash>
 #include <QApplication>
 #include <QCommandLineParser>
 
+#include "config-drawy.hpp"
 #include "drawycommandlineparser.h"
 #include "window/window.hpp"
 
@@ -23,17 +25,27 @@ int main(int argc, char *argv[])
     QApplication::setStyle(u"Fusion"_s);
     a.setWindowIcon(QIcon(u":/drawy/drawy.svg"_s));
 
+    KAboutData aboutData(u"drawy"_s,
+                         QObject::tr("drawy"),
+                         QStringLiteral(DRAWY_VERSION),
+                         QObject::tr("Your handy, infinite, brainstorming tool!"),
+                         KAboutLicense::GPL_V3,
+                         QObject::tr("(c) 2025-%1 drawy authors").arg(u"2026"_s));
+    aboutData.addAuthor(QObject::tr("Prayag Jain"), QObject::tr("Maintainer"), u"prayagjain2@gmail.com"_s);
+    aboutData.addAuthor(QObject::tr("Laurent Montel"), QObject::tr("Developer"), u"montel@kde.org"_s);
+
     KCrash::initialize();
 
     QCommandLineParser parser;
+    aboutData.setupCommandLine(&parser);
     const DrawyCommandLineParser commandLineParser(&parser);
     parser.addPositionalArgument(QStringLiteral("file"), QObject::tr("An optional argument 'file' "), QStringLiteral("[file]"));
 
-    parser.addHelpOption();
     parser.addVersionOption();
     parser.setApplicationDescription(QObject::tr("Your handy, infinite, brainstorming tool!"));
 
     parser.process(a);
+    aboutData.processCommandLine(&parser);
 
     MainWindow w;
     if (parser.isSet(commandLineParser.optionParserFromEnum(DrawyCommandLineParser::OptionParser::FullScreen))) {
