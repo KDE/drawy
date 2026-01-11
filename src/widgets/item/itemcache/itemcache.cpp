85eb46898f61c78478ab3ef723db4fb412d7cab8
Prayag Jain <prayagjain2@gmail.com>
2026-01-11 15:28:14 +0530

    fix: query tiles correctly

--- src/widgets/item/itemcache/itemcache.cpp
+++ src/widgets/item/itemcache/itemcache.cpp
@@ -12,7 +12,7 @@
 #include "../../data-structures/cachegrid.hpp"
 #include "../item.hpp"
 
-void ItemCache::drawCached(QPainter &painter, const std::shared_ptr<Item> &item, const QPointF &offset)
+void ItemCache::drawCached(QPainter &painter, const std::shared_ptr<Item> &item, const QRect &queryRegion, const QPointF &offset)
 {
     Q_ASSERT(item->needsCaching());
 
@@ -20,39 +20,36 @@ void ItemCache::drawCached(QPainter &painter, const std::shared_ptr<Item> &item,
     auto &transformer{context->spatialContext().coordinateTransformer()};
     auto &canvas{context->renderingContext().canvas()};
 
-    QRectF boundingBox{item->boundingBox()};
+    const QRectF boundingBox{item->boundingBox()};
     QTransform transform{item->transformObj()};
-    // account for initial translation
-    transform.translate(boundingBox.topLeft().x(), boundingBox.topLeft().y());
-
-    const QPointF worldOffset{context->spatialContext().offsetPos()};
-    const QRectF worldViewport(worldOffset, transformer.viewToWorld(canvas.dimensions()));
-    const QRectF gridViewport{transformer.worldToGrid(worldViewport)};
-    const QRectF transformedGridViewport{transformer.worldToGrid(transform.inverted().mapRect(worldViewport))};
+    transform.translate(boundingBox.topLeft().x(), boundingBox.topLeft().y()); // account for the initial translation
+                                                                               //
+    const QRectF transformedQueryRegion{transformer.worldToGrid(transform.inverted().mapRect(queryRegion))};
 
     // Check if item is not already cached
     if (m_cacheGrids.find(item) == m_cacheGrids.end()) {
         QSizeF itemSize{transformer.worldToGrid(boundingBox.size())};
         QSizeF maxCellSize{Common::maxItemCacheCellSize.toSizeF()};
 
-        // If the item is smaller than the max cell size, we use just one cell of
+        const int rows{static_cast<int>(std::ceil(transformedQueryRegion.width() / maxCellSize.width())) + 1};
+        const int cols{static_cast<int>(std::ceil(transformedQueryRegion.height() / maxCellSize.height())) + 1};
+        const int maxCellCount{rows * cols};
+
+        // If the item is smaller than the max cell size, we use cells of
         // size equal to its bounding box
-        int maxCellCount{1};
         QSize cellSize{transformer.worldToGrid(boundingBox.size().toSize())};
         if (itemSize.width() / maxCellSize.width() > maxCellSize.height() / itemSize.height()) {
-            int rows{static_cast<int>(std::ceil(gridViewport.width() / maxCellSize.width())) + 1};
-            int cols{static_cast<int>(std::ceil(gridViewport.height() / maxCellSize.height())) + 1};
-
-            maxCellCount = rows * cols;
             cellSize = Common::maxItemCacheCellSize;
         }
 
+        const QRectF gridAdjustedBoundingBox{transformer.worldToGrid(boundingBox.translated(-boundingBox.topLeft()))};
         m_cacheGrids[item] = std::make_unique<CacheGrid>(maxCellCount, cellSize);
+        m_cacheGrids[item]->setBounds(gridAdjustedBoundingBox.toRect());
     }
 
     const qreal zoom{context->renderingContext().zoomFactor()};
-    const QRect queryRegion{transformedGridViewport.intersected(transformer.worldToGrid(boundingBox.translated(-boundingBox.topLeft()))).toRect()};
-    auto visibleCells{m_cacheGrids[item]->queryCells(queryRegion)};
+    auto visibleCells{m_cacheGrids[item]->queryCells(transformedQueryRegion.toRect())};
+
     for (const auto &cell : visibleCells) {
         if (cell->dirty()) {
             cell->paint([&](QPainter &painter) -> void {
@@ -64,13 +61,14 @@ void ItemCache::drawCached(QPainter &painter, const std::shared_ptr<Item> &item,
 
         painter.drawPixmap(transformer.worldToGrid(boundingBox.topLeft()) + cell->rect().topLeft() - offset, cell->pixmap());
 
-        // painter.save();
-        // QPen pen{};
-        // pen.setColor(Qt::red);
-        // painter.setPen(pen);
-        // painter.drawRect(cell->rect().toRectF().translated(transformer.worldToGrid(boundingBox.topLeft()) - offset));
-        // painter.drawText(transformer.worldToGrid(boundingBox.topLeft()) + cell->rect().topLeft() - offset, QString::asprintf("(%d, %d)", cell->point().x(),
-        // cell->point().y())); painter.restore();
+        painter.save();
+        QPen pen{};
+        pen.setColor(Qt::red);
+        painter.setPen(pen);
+        painter.drawRect(cell->rect().toRectF().translated(transformer.worldToGrid(boundingBox.topLeft()) - offset));
+        painter.drawText(transformer.worldToGrid(boundingBox.topLeft()) + cell->rect().topLeft() - offset,
+                         QString::asprintf("(%d, %d)", cell->point().x(), cell->point().y()));
+        painter.restore();
     }
 }
 
