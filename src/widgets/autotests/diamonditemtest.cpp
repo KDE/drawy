8faa13028d5066e0fbd9e981976f9d4aeea6122d
Laurent Montel <montel@kde.org>
2026-01-16 08:36:46 +0100

    Add diamondautotest

---
+++ src/widgets/autotests/diamonditemtest.cpp
@@ -0,0 +1,37 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+ *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+
+#include "diamonditemtest.hpp"
+#include "drawy_autotest_helper.hpp"
+#include "item/diamond.hpp"
+#include <QJsonObject>
+#include <QTest>
+QTEST_MAIN(DiamondItemTest)
+using namespace Qt::Literals::StringLiterals;
+DiamondItemTest::DiamondItemTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void DiamondItemTest::shouldHaveDefaultValues()
+{
+    const DiamondItem i;
+    QCOMPARE(i.type(), Item::Type::Diamond);
+    QVERIFY(!i.id().isEmpty());
+}
+
+void DiamondItemTest::shouldSerializeDefaultValue()
+{
+    DiamondItem f;
+    // Need to have an known id
+    f.setId("acff679ae3c14260b56ef00f1d354883"_ba);
+    const QJsonObject obj = f.serialize();
+    const QJsonDocument doc(obj);
+    const QByteArray ba = doc.toJson();
+    AutoTestHelper::compareFile(u"/diamond/"_s, ba, u"defaultvalue"_s);
+}
+
+#include "moc_diamonditemtest.cpp"
