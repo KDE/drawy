469e90842ffc9d1d1ea090a486b2685461708d59
Laurent Montel <montel@kde.org>
2026-01-21 13:42:30 +0100

    Add alignitemcommand command

---
+++ src/widgets/command/alignitemcommand.cpp
@@ -0,0 +1,45 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+#include "alignitemcommand.h"
+#include "context/applicationcontext.hpp"
+#include "context/coordinatetransformer.hpp"
+#include "context/renderingcontext.hpp"
+#include "context/selectioncontext.hpp"
+#include "context/spatialcontext.hpp"
+#include "data-structures/cachegrid.hpp"
+#include "item/item.hpp"
+
+AlignItemCommand::AlignItemCommand(QList<std::shared_ptr<Item>> items, AlignItemCommand::Alignment alignment)
+    : ItemCommand{std::move(items)}
+    , m_alignment(alignment)
+{
+}
+
+void AlignItemCommand::execute(ApplicationContext *context)
+{
+    auto &transformer{context->spatialContext()->coordinateTransformer()};
+    auto &cacheGrid{context->renderingContext()->cacheGrid()};
+
+    QRectF dirtyRegion;
+    for (const auto &item : std::as_const(m_items)) {
+        dirtyRegion |= item->boundingBox();
+    }
+    for (const auto &item : std::as_const(m_items)) {
+        cacheGrid.markDirty(transformer.worldToGrid(item->boundingBox()).toRect());
+        cacheGrid.markDirty(transformer.worldToGrid(item->boundingBox()).toRect());
+    }
+}
+
+void AlignItemCommand::undo(ApplicationContext *context)
+{
+    auto &transformer{context->spatialContext()->coordinateTransformer()};
+    auto &cacheGrid{context->renderingContext()->cacheGrid()};
+
+    for (const auto &item : std::as_const(m_items)) {
+        cacheGrid.markDirty(transformer.worldToGrid(item->boundingBox()).toRect());
+        cacheGrid.markDirty(transformer.worldToGrid(item->boundingBox()).toRect());
+    }
+}
---
+++ src/widgets/command/alignitemcommand.h
@@ -0,0 +1,32 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+#pragma once
+#include "itemcommand.hpp"
+#include <QList>
+#include <QPointF>
+class ApplicationContext;
+class AlignItemCommand : public ItemCommand
+{
+public:
+    enum class Alignment : uint8_t {
+        Unknow = 0,
+        AlignToTop,
+        AlignToBottom,
+        AlignToRight,
+        AlignToLeft,
+        AlignHorizontalCenter,
+        AlignVerticalCenter,
+    };
+
+    explicit AlignItemCommand(QList<std::shared_ptr<Item>> items, AlignItemCommand::Alignment alignment);
+
+    void execute(ApplicationContext *context) override;
+    void undo(ApplicationContext *context) override;
+
+private:
+    const AlignItemCommand::Alignment m_alignment = AlignItemCommand::Alignment::Unknow;
+    QList<QPointF> m_moveToPoint;
+};
