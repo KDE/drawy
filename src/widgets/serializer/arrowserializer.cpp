e6a1b530377261374f42c9e43d85f6abbe028708
Nikolay Kochulin <basiqueevangelist@yandex.ru>
2026-01-24 22:25:19 +0300

    Fix arrow serialization

--- src/widgets/serializer/arrowserializer.cpp
+++ src/widgets/serializer/arrowserializer.cpp
@@ -19,6 +19,6 @@ QJsonObject ArrowSerializer::serialize() const
     const ArrowItem *arrow = dynamic_cast<const ArrowItem *>(mItem);
     QJsonObject obj = PolygonSerializer::serialize();
     obj[u"startArrow"_s] = ArrowUtils::convertArrowTypeEnumToString(arrow->startArrow());
-    obj[u"endArrow"_s] = ArrowUtils::convertArrowTypeEnumToString(arrow->startArrow());
+    obj[u"endArrow"_s] = ArrowUtils::convertArrowTypeEnumToString(arrow->endArrow());
     return obj;
 }
