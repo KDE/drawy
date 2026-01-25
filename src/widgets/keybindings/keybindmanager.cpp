a80f1323c239703f598f68ddf35497ddffc12638
Laurent Montel <montel@kde.org>
2026-01-25 10:27:08 +0100

    Fix include

--- src/widgets/keybindings/keybindmanager.cpp
+++ src/widgets/keybindings/keybindmanager.cpp
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #include "keybindmanager.hpp"
-#include <kactioncollection.h>
+#include <KActionCollection>
 
 KeybindManager::KeybindManager(QObject *parent)
     : QObject{parent}
