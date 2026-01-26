fadff5c0c6bf5871da224bebfe1e5441ab99740f
Laurent Montel <montel@kde.org>
2026-01-26 12:24:28 +0100

    Rename hezader extension

--- src/widgets/command/alignitemcommand.cpp
+++ src/widgets/command/alignitemcommand.cpp
@@ -3,7 +3,7 @@
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
-#include "alignitemcommand.h"
+#include "alignitemcommand.hpp"
 #include "context/applicationcontext.hpp"
 #include "context/coordinatetransformer.hpp"
 #include "context/renderingcontext.hpp"
