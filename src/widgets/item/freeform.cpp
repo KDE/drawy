12a5c2f47a809aa5aed64b249a962bfcea936801
Laurent Montel <montel@kde.org>
2026-02-03 13:23:02 +0100

    Make compile with qt < 6.10
    BUG: 515291

--- src/widgets/item/freeform.cpp
+++ src/widgets/item/freeform.cpp
@@ -61,7 +61,9 @@ void FreeformItem::finalizeStroke()
     const qreal thickness{property(Property::Type::StrokeWidth).value<qreal>()};
 
     m_path = Common::Utils::Freehand::getStroke(m_points, m_pressures, m_simulatePressure, thickness);
+#if QT_VERSION >= QT_VERSION_CHECK(6, 10, 0)
     m_path.setCachingEnabled(true);
+#endif
 
     m_boundingBox = m_path.boundingRect().normalized();
 }
