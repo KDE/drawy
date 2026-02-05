e3fbc62a36e9bd6777b1f1f8007751b6637b4a0e
Prayag Jain <prayagjain2@gmail.com>
2026-02-06 00:24:57 +0530

    chore: add copyright

--- src/widgets/autotests/autosavewidgettest.cpp
+++ src/widgets/autotests/autosavewidgettest.cpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 Prayag Jain <prayagjain2@gmail.com>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #include "autosavewidgettest.hpp"
 #include "dialog/general/autosavewidget.hpp"
 #include <QCheckBox>
