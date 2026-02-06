6739d2abed84d6b5e20ea39e09fd932bcc1f6f26
Nikolay Kochulin <basiqueevangelist@yandex.ru>
2026-02-06 19:04:48 +0300

    fix: make group round-tripping work

--- src/widgets/serializer/groupdeserializer.cpp
+++ src/widgets/serializer/groupdeserializer.cpp
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 #include "groupdeserializer.hpp"
+#include "item/itemutils.hpp"
 #include <QJsonArray>
 #include <QJsonObject>
 using namespace Qt::Literals::StringLiterals;
@@ -17,9 +18,22 @@ GroupDeserializer::~GroupDeserializer() = default;
 void GroupDeserializer::deserialize(const QJsonObject &obj)
 {
     ItemDeserializer::deserialize(obj);
+
+    GroupItem *groupItem = static_cast<GroupItem *>(mItem);
+
     const QJsonArray items = obj[u"items"_s].toArray();
+
+    QList<std::shared_ptr<Item>> itemsToAdd;
+    itemsToAdd.reserve(items.size());
+
     for (const auto &val : items) {
-        const QJsonObject item = val.toObject();
-        // Deserialize each item
+        const QJsonObject itemObj = val.toObject();
+        const Item::Type type{ItemUtils::convertItemTypeStringToEnum(ItemDeserializer::value(itemObj, u"type"_s).toString())};
+        const std::shared_ptr<Item> item = ItemUtils::createItemFromType(type);
+        item->deserialize(itemObj);
+
+        itemsToAdd.append(item);
     }
+
+    groupItem->setItems(itemsToAdd);
 }
