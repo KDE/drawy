0eb5a15d05aa185956387a68a875926ab49bd147
Laurent Montel <montel@kde.org>
2026-03-09 13:24:02 +0100

    Add icons

--- src/widgets/properties/widgets/stokestylewidget.cpp
+++ src/widgets/properties/widgets/stokestylewidget.cpp
@@ -27,16 +27,14 @@ StokeStyleWidget::StokeStyleWidget(QWidget *parent)
     solidButton->setProperty("stroke-style", u"Solid"_s);
     solidButton->setToolTip(i18nc("@info:tooltip", "Solid"));
     solidButton->setCheckable(true);
-    // Laurent: wait for icons (from kde team)
-    solidButton->setIcon(QIcon::fromTheme(u"line_solid"_s));
+    solidButton->setIcon(QIcon::fromTheme(u"://icons/stroke-style-solid.svg"_s));
     m_group->addButton(solidButton);
 
     auto dashLineButton{new ButtonActionsWidget(m_widget)};
     dashLineButton->setObjectName(u"dashLineButton"_s);
     dashLineButton->setProperty("stroke-style", u"DashLine"_s);
     dashLineButton->setToolTip(i18nc("@info:tooltip", "Dashed"));
-    // Laurent: wait for icons (from kde team)
-    dashLineButton->setIcon(QIcon::fromTheme(u"line_dashed"_s));
+    dashLineButton->setIcon(QIcon::fromTheme(u"://icons/stroke-style-dashed.svg"_s));
     dashLineButton->setCheckable(true);
     m_group->addButton(dashLineButton);
 
@@ -44,8 +42,7 @@ StokeStyleWidget::StokeStyleWidget(QWidget *parent)
     dotLineButton->setObjectName(u"dotLineButton"_s);
     dotLineButton->setProperty("stroke-style", u"DotLine"_s);
     dotLineButton->setToolTip(i18nc("@info:tooltip", "Dotted"));
-    // Laurent: wait for icons (from kde team)
-    dotLineButton->setIcon(QIcon::fromTheme(u"line_dotter"_s));
+    dotLineButton->setIcon(QIcon::fromTheme(u"://icons/stroke-style-dotted.svg"_s));
     dotLineButton->setCheckable(true);
     m_group->addButton(dotLineButton);
 
