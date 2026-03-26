de10ca308bcf8ef14f32572b11c8e9428cc77bb7
Aviral Singh <aviral02singh@gmail.com>
2026-03-26 15:20:03 +0530

    fix: snap opacity slider to 10% steps to reduce undo spam

--- src/widgets/autotests/opacitywidgettest.cpp
+++ src/widgets/autotests/opacitywidgettest.cpp
@@ -5,6 +5,8 @@
  */
 #include "opacitywidgettest.hpp"
 #include "properties/widgets/opacitywidget.hpp"
+#include <QLabel>
+#include <QSignalSpy>
 #include <QSlider>
 #include <QTest>
 using namespace Qt::Literals::StringLiterals;
@@ -25,9 +27,53 @@ void OpacityWidgetTest::shouldHaveDefaultValues()
     auto slider = w.widget()->findChild<QSlider *>(u"slider"_s);
     QVERIFY(slider);
     QCOMPARE(slider->objectName(), u"slider"_s);
-    QCOMPARE(slider->minimum(), 1);
-    QCOMPARE(slider->maximum(), 255);
-    QCOMPARE(slider->value(), 255);
+    QCOMPARE(slider->minimum(), 0);
+    QCOMPARE(slider->maximum(), 100);
+    QCOMPARE(slider->singleStep(), 10);
+    QCOMPARE(slider->pageStep(), 10);
+    QCOMPARE(slider->tickInterval(), 10);
+    QCOMPARE(slider->value(), 100);
+
+    auto label = w.widget()->findChild<QLabel *>(u"label"_s);
+    QVERIFY(label);
+    QCOMPARE(label->text(), u"100"_s);
+}
+
+void OpacityWidgetTest::shouldSnapOpacityValuesToDiscreteSteps()
+{
+    OpacityWidget w;
+    QSignalSpy spy(&w, &PropertyWidget::changed);
+
+    auto slider = w.widget()->findChild<QSlider *>(u"slider"_s);
+    QVERIFY(slider);
+
+    slider->setValue(56);
+    QCOMPARE(slider->value(), 60);
+    QCOMPARE(spy.count(), 1);
+    QCOMPARE(w.value().value<int>(), 153);
+
+    auto label = w.widget()->findChild<QLabel *>(u"label"_s);
+    QVERIFY(label);
+    QCOMPARE(label->text(), u"60"_s);
+}
+
+void OpacityWidgetTest::shouldNotEmitChangedWhenSettingValueProgrammatically()
+{
+    OpacityWidget w;
+    QSignalSpy spy(&w, &PropertyWidget::changed);
+
+    w.setValue(128);
+    QCOMPARE(spy.count(), 0);
+
+    auto slider = w.widget()->findChild<QSlider *>(u"slider"_s);
+    QVERIFY(slider);
+    QCOMPARE(slider->value(), 50);
+
+    auto label = w.widget()->findChild<QLabel *>(u"label"_s);
+    QVERIFY(label);
+    QCOMPARE(label->text(), u"50"_s);
+
+    QCOMPARE(w.value().value<int>(), 128);
 }
 
 #include "moc_opacitywidgettest.cpp"
