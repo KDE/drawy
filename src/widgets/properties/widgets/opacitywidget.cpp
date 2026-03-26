38e5b3c8562137cc959af53211b761174bae4a14
Aviral Singh <aviral02singh@gmail.com>
2026-03-26 15:20:03 +0530

    fix: snap opacity slider to 10% steps to reduce undo spam

--- src/widgets/properties/widgets/opacitywidget.cpp
+++ src/widgets/properties/widgets/opacitywidget.cpp
@@ -7,8 +7,44 @@
 #include <KLocalizedString>
 #include <QHBoxLayout>
 #include <QLabel>
+#include <QSignalBlocker>
 #include <QSlider>
+#include <algorithm>
 using namespace Qt::Literals::StringLiterals;
+
+namespace
+{
+constexpr int minimumAlpha = 1;
+constexpr int maximumAlpha = 255;
+constexpr int minimumPercent = 0;
+constexpr int maximumPercent = 100;
+constexpr int percentStep = 10;
+
+int alphaToPercent(int alpha)
+{
+    const int boundedAlpha = std::clamp(alpha, minimumAlpha, maximumAlpha);
+    return qRound((boundedAlpha * 100.0) / maximumAlpha);
+}
+
+int percentToAlpha(int percent)
+{
+    const int boundedPercent = std::clamp(percent, minimumPercent, maximumPercent);
+    if (boundedPercent == minimumPercent) {
+        return minimumAlpha;
+    }
+    if (boundedPercent == maximumPercent) {
+        return maximumAlpha;
+    }
+    return std::clamp(qRound((boundedPercent / 100.0) * maximumAlpha), minimumAlpha, maximumAlpha);
+}
+
+int snapPercent(int percent)
+{
+    const int boundedPercent = std::clamp(percent, minimumPercent, maximumPercent);
+    return ((boundedPercent + (percentStep / 2)) / percentStep) * percentStep;
+}
+}
+
 OpacityWidget::OpacityWidget(QWidget *parent)
     : PropertyWidget(parent)
     , mSlider(new QSlider(parent))
@@ -19,28 +55,43 @@ OpacityWidget::OpacityWidget(QWidget *parent)
     layout->setContentsMargins({});
 
     mSlider->setObjectName(u"slider"_s);
-    mSlider->setMinimum(1);
-    mSlider->setMaximum(255);
-    mSlider->setValue(255);
+    mSlider->setMinimum(minimumPercent);
+    mSlider->setMaximum(maximumPercent);
+    mSlider->setSingleStep(percentStep);
+    mSlider->setPageStep(percentStep);
+    mSlider->setTickInterval(percentStep);
+    mSlider->setValue(maximumPercent);
     mSlider->setOrientation(Qt::Horizontal);
 
     auto label = new QLabel(m_widget);
     label->setObjectName(u"label"_s);
-    label->setText(QString::number(100));
+    label->setText(QString::number(maximumPercent));
 
     layout->addWidget(mSlider);
     layout->addWidget(label);
     m_widget->hide();
 
     connect(mSlider, &QSlider::valueChanged, this, [this, label](int v) {
-        label->setText(QString::number(static_cast<int>((v / 255.0) * 100)));
+        const int snappedPercent = snapPercent(v);
+        if (snappedPercent != v) {
+            const QSignalBlocker blocker(mSlider);
+            mSlider->setValue(snappedPercent);
+        }
+
+        label->setText(QString::number(snappedPercent));
         Q_EMIT changed(value());
     });
 }
 
 void OpacityWidget::setValue(const QVariant &value)
 {
-    mSlider->setValue(value.toInt());
+    const int snappedPercent = snapPercent(alphaToPercent(value.toInt()));
+    const QSignalBlocker blocker(mSlider);
+    mSlider->setValue(snappedPercent);
+    auto label = m_widget->findChild<QLabel *>(u"label"_s);
+    if (label) {
+        label->setText(QString::number(snappedPercent));
+    }
 }
 
 QString OpacityWidget::name() const
@@ -50,7 +101,7 @@ QString OpacityWidget::name() const
 
 const Property OpacityWidget::value() const
 {
-    return Property{mSlider->value(), Property::Type::Opacity};
+    return Property{percentToAlpha(mSlider->value()), Property::Type::Opacity};
 }
 
 #include "moc_opacitywidget.cpp"
