2693984776ed777351cfe31e4edc54a4ac2e84f7
Abdelhadi Wael <waelhadi380@gmail.com>
2026-04-03 03:14:26 +0200

    fix: export now recognizes background color

--- src/widgets/mime/svgmimehandler.cpp
+++ src/widgets/mime/svgmimehandler.cpp
@@ -3,10 +3,14 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #include "mime/svgmimehandler.hpp"
+#include "common/constants.hpp"
+#include "drawyglobalconfig.h"
 #include "item/item.hpp"
 #include "serializer/svgserializer.hpp"
 #include <QByteArray>
+#include <QGuiApplication>
 #include <QMimeData>
+#include <QPalette>
 #include <QXmlStreamWriter>
 #include <memory>
 
@@ -17,7 +21,9 @@ void SvgMimeHandler::contributeData(QMimeData &mimeData, const QList<std::shared
     QByteArray svgData;
     QXmlStreamWriter svgWriter(&svgData);
 
-    SvgSerializer::writeSvg(svgWriter, selectedItems);
+    const bool isDark = QGuiApplication::palette().color(QPalette::Window).lightnessF() < 0.5;
+    const QColor backgroundColor = isDark ? DrawyGlobalConfig::backgroundColorDark() : DrawyGlobalConfig::backgroundColorLight();
+    SvgSerializer::writeSvg(svgWriter, selectedItems, backgroundColor);
 
     mimeData.setData(u"image/svg+xml"_s, svgData);
 }
