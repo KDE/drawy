b95c9597712f50caae43bd93146d46a2e8d585e4
Laurent Montel <montel@kde.org>
2026-04-29 09:11:45 +0200

    Add page class

---
+++ src/widgets/page/page.cpp
@@ -0,0 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Laurent Montel <montel@kde.org>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+#include "page.hpp"
+
+Page::Page() = default;
+Page::~Page() = default;
